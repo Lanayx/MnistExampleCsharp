61e0346076a8c257394b72c537298669b11417ac
odind <odind@ODIN>
2016-08-21 00:40:51 +0300

    Initial commit

---
+++ MnistExampleCsharp/Network.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnistExampleCsharp
+{
+    public class Network
+    {
+        public int num_layers;
+        public int[] sizes;
+        public double[][][] biases;
+        public double[][][] weights;
+
+        public Network(int[] sizes)//2 3 4
+        {
+            var r = new Random(0);
+            this.num_layers = sizes.Length;
+            this.sizes = sizes;
+            this.biases = sizes.Skip(1).Select(x => (new int[x])
+                .Select(e =>new [] { GetRandomNumber(-3, 3, r) }).ToArray()).ToArray();
+
+            this.weights = new double[sizes.Length-1][][];
+            for (int i = 0; i < sizes.Length-1; i++)
+            {
+                this.weights[i] = Enumerable.Range(0, sizes[i + 1])
+                    .Select(e => Enumerable.Range(0, sizes[i]).Select(e1 => GetRandomNumber(-3, 3, r)).ToArray()).ToArray();
+            }
+        }
+
+        public double GetRandomNumber(double minimum, double maximum, Random random)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
---
+++ MnistExampleCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MnistExampleCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var net = new Network(new [] {1, 2, 1});
+            Console.WriteLine("Biases");
+            Console.WriteLine(JsonConvert.SerializeObject(net.biases));
+
+            Console.WriteLine("Weights");
+            Console.WriteLine(JsonConvert.SerializeObject(net.weights));
+        }
+    }
+}
